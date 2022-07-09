095f600ff1189c76832b5cb7698ce565a8fd960c
jmcreynolds <jeffsmcreynolds@gmail.com>
2022-07-08 19:07:54 -0500

    new stuff

--- src/handlers/users.js
+++ src/handlers/users.js
@@ -1,10 +1,6 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
-
-import middy from '@middy/core';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
+import commonMiddleware from '../../lib/commonMiddleware';
 import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -35,9 +31,4 @@ async function users(event, context) {
   };
 }
 
-export const handler = middy(users)
-.use(httpJsonBodyParser())
-.use(httpEventNormalizer())
-.use(httpErrorHandler());
-
-
+export const handler = commonMiddleware(users);
